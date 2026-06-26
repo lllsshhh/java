b42f6a0cc313e7e15dc5cde626d899de7a72ff78
lllsshhh <qaz31378@gmail.com>
2026-06-26 21:58:45 +0900

    Update Sort.java

--- Sort.java
+++ Sort.java
@@ -1,10 +1,38 @@
-package jump2java;
+package test3;
+import java.util.Scanner;
 
 public class Sort {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.print("첫번째 정수: ");
+		int a = sc.nextInt();
+		System.out.print("두번째 정수: ");
+		int b = sc.nextInt();
+		System.out.print("세번째 정수: ");
+		int c = sc.nextInt();
+		int temp;
+		
+		if (a > b) {
+			temp = a;
+			a = b;
+			b = temp;
+		}
+		
+		if (a > c) {
+			temp = a;
+			a = c;
+			c = temp;
+		}
+		if (b > c) {
+			temp = b;
+			b = c;
+			c = temp;
+		}
+		System.out.println("오름차순 정렬: "+a+","+b+","+c);
+		sc.close();
 	}
-
 }
+
+
+
+
